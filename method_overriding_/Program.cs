c9b5784c160bc5447be18ffb2a33378714d4bdd2
admin <admin@CHETUIWK1860>
2022-07-27 13:49:03 +0530

    Add project files.

---
+++ method_overriding_/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace method_overriding_
+{
+    class Employee
+    {
+        public virtual void  Show(int a ,int b)
+        {
+            Console.WriteLine("this method is parent class="+ a +" " + b);
+        }
+
+    }
+
+    class Student : Employee
+    {
+        public override void Show(int a, int b)
+        {
+            base.Show(a, b);
+            Console.WriteLine("this method is child class ="+ a + " " + b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.Show(1, 2);
+            Console.ReadLine();
+        }
+    }
+}
